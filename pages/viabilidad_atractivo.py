a4b0f5692e09ee924d1127d253f693b9e774d640
Hermilo Cortes <hermilocg@hotmail.com>
2026-06-17 18:22:06 -0600

    agrega graficas de dispersion de factores

--- pages/viabilidad_atractivo.py
+++ pages/viabilidad_atractivo.py
@@ -1,7 +1,123 @@
 import streamlit as st
 from utils import factores_descripcion
+import polars as pl 
+import altair as alt 
+import pandas as pd 
 
 st.title(":material/bar_chart: Factores de Viabilidad y Atractivo")
+
+## Carga datos
+cdata_intensivo = pl.read_csv("datos/honduras_intensivo_viabilidad_atractivo.csv")
+cdata_extensivo = pl.read_csv("datos/honduras_extensivo_viabilidad_atractivo.csv")
+
+def plot_industrias(criterio):
+
+
+    # Define your custom mapping arrays
+    group_domains = ['Actividades de servicios administrativos y de apoyo',
+    'Actividades profesionales, científicas y técnicas',
+    'Comercio al por mayor y al por menor; reparación de vehículos automotores y motocicletas',
+    'Construcción',
+    'Explotación de minas y canteras',
+    'Industrias manufactureras',
+    'Información y comunicaciones',
+    'Suministro de agua; evacuación de aguas residuales, gestión de desechos y descontaminación',
+    'Suministro de electricidad, gas, vapor y aire acondicionado',
+    'Transporte y almacenamiento']
+
+    color_range = [
+        "#1f77b4", 
+        "#ff7f0e",
+        "#2ca02c",
+        "#d62728",
+        "#9467bd",
+        "#FFFF00",
+        "#e377c2",
+        "#7f7f7f",
+        "#bcbd22",
+        "#17becf",
+    ]
+
+
+    if criterio == "Margen Intensivo": 
+
+        plot_intensivo = alt.Chart(
+            cdata_intensivo    
+        ).mark_circle(
+                    opacity=0.99,
+                    stroke='black',
+                    strokeWidth=1.2,
+                    strokeOpacity=0.9, 
+                    size=200,     
+                ).encode(
+            x=alt.X('topsis_viabilidad').scale(zero=False).title("Viabilidad"),
+            y=alt.Y('topsis_atractivo').scale(zero=False).title("Atractivo"),#.scale(type ="log"),
+            color = alt.Color("seccion_titulo", scale=alt.Scale(domain=group_domains, range=color_range)).title("Sección"),
+            #size = alt.Size("OBS_VALUE").scale(type ="log").title("Empleo"),
+            tooltip=[
+
+                    alt.Tooltip('nombre_actividad', title='Actividad'), 
+                    alt.Tooltip('division_titulo', title='División CIIU Rev 4'),
+                    alt.Tooltip('OBS_VALUE', title='Empleo'),
+            ] 
+
+        )
+
+        # Create a horizontal line at y = -1.14
+        rule_atractivo = alt.Chart(pd.DataFrame({'y': [cdata_intensivo["topsis_atractivo"].mean()]})).mark_rule(color='red').encode(y='y:Q')
+        rule_viabilidad = alt.Chart(pd.DataFrame({'x': [cdata_intensivo["topsis_viabilidad"].mean()]})).mark_rule(color='red').encode(x='x:Q')
+
+
+        plot_intensivo = (plot_intensivo + rule_atractivo + rule_viabilidad).properties(
+        #plot_intensivo.properties(
+                title=alt.TitleParams(
+                    "Diagrama Viabilidad-Atractivo",
+                    subtitle="Margen Intensivo",
+                    subtitleColor="gray"
+                )
+        )
+
+        st.altair_chart(plot_intensivo, theme=None, use_container_width=True)
+    elif criterio == "Margen Extensivo":
+
+
+        plot_extensivo = alt.Chart(
+            cdata_extensivo    
+        ).mark_circle(
+                    opacity=0.99,
+                    stroke='black',
+                    strokeWidth=1.2,
+                    strokeOpacity=0.9, 
+                    size=200,     
+                ).encode(
+            x=alt.X('topsis_viabilidad').scale(zero=False).title("Viabilidad"),
+            y=alt.Y('topsis_atractivo').scale(zero=False).title("Atractivo"),#.scale(type ="log"),
+            color = alt.Color("seccion_titulo", scale=alt.Scale(domain=group_domains, range=color_range)).title("Sección"),
+            #size = alt.Size("OBS_VALUE").scale(type ="log").title("Empleo"),
+            tooltip=[
+
+                    alt.Tooltip('nombre_actividad', title='Actividad'), 
+                    alt.Tooltip('division_titulo', title='División CIIU Rev 4'),
+                    alt.Tooltip('OBS_VALUE', title='Empleo'),
+            ] 
+
+        )
+
+        # Create a horizontal line at y = -1.14
+        rule_extensivo_atractivo = alt.Chart(pd.DataFrame({'y': [cdata_extensivo["topsis_atractivo"].mean()]})).mark_rule(color='red').encode(y='y:Q')
+        rule_extensivo_viabilidad = alt.Chart(pd.DataFrame({'x': [cdata_extensivo["topsis_viabilidad"].mean()]})).mark_rule(color='red').encode(x='x:Q')
+
+
+        plot_extensivo = (plot_extensivo + rule_extensivo_atractivo + rule_extensivo_viabilidad).properties(
+        #plot_intensivo.properties(
+                title=alt.TitleParams(
+                    "Diagrama Viabilidad-Atractivo",
+                    subtitle="Margen Extensivo",
+                    subtitleColor="gray"
+                )
+        )
+
+        st.altair_chart(plot_extensivo, theme=None, use_container_width=True)
 
 
 
@@ -75,5 +191,26 @@ with tab2:
             key="ex_category",
             bind="query-params",
         )
+    
+    st.markdown(
+        """
+    Posterior a la identificación de industrias clave dentro del espectro de alternativas económicas
+    se aplicó la técnica **TOPSIS (Technique for Order of Preference by Similarity to Ideal
+    Solution)**. Esta técnica pertenece al conjunto de métodos conocidos como **Decisión
+    Multicriterio**, que se utiliza para **evaluar y ordenar múltiples opciones frente a un conjunto
+    de criterios de valoración**. 
+    
+    TOPSIS destaca por su capacidad para determinar la cercanía de una alternativa a la solución ideal, que se define como la instancia donde cada criterio alcanza
+    su valor óptimo o solución ideal.
 
-        st.markdown(f"## {selected_criteria}")
+
+    TOPSIS estima un ranking normalizado entre 0 y 1, donde 1 indica una coincidencia perfecta con la solución ideal. Este ranking proporciona una jerarquía clara de priorización industrial, develando aquellas industrias con el mayor potencial estratégico.
+
+    Los criterios seleccionados para definir esta solución ideal corresponden a los factores de viabilidad y atractivo.
+ 
+
+        """
+    )
+
+    st.markdown(f"## {selected_criteria}")
+    plot_industrias(selected_criteria)
